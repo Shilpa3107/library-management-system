66d9003a1506d0fb47b6c3e444aa280457f69fdb
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-03-28 04:55:18 +0530

    Set the attributes to not null

--- library/src/main/java/com/library/model/Book.java
+++ library/src/main/java/com/library/model/Book.java
@@ -2,6 +2,8 @@ package com.library.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Getter
 @Setter
@@ -12,17 +14,23 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
+
+    @NotBlank(message = "Title cannot be empty")
     @Column(nullable = false, unique = true)
     private String title;
 
+    @NotBlank(message = "Author cannot be empty")
     @Column(nullable = false)
     private String author;
 
+    @NotBlank(message = "Genre cannot be empty")
     @Column(nullable = false)
     private String genre;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private AvailabilityStatus status;
 }
