7feae0eb953f668def4e2d0ff8dd52bc1f9bf343
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-03-28 04:56:36 +0530

    Make the crud operations to ui methods

--- library/src/main/java/com/library/controller/HomeController.java
+++ library/src/main/java/com/library/controller/HomeController.java
@@ -1,15 +1,39 @@
 package com.library.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import com.library.model.Book;
+import com.library.service.BookService;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/")
     public String home() {
-        return "Welcome to the Library Management System!";
+        return "index";
+    }
+
+    @GetMapping("/books")
+    public String viewBooks(Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
+        return "books/list"; 
+    }
+
+    @GetMapping("/addBook")
+    public String addBookPage(Model model) {
+        model.addAttribute("book", new Book());
+        return "books/add"; 
+    }
+
+    @PostMapping("/addBook")
+    public String addBook(@ModelAttribute Book book) {
+        bookService.addBook(book);
+        return "redirect:/books"; 
     }
 }
